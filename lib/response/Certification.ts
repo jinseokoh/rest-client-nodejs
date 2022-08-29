e4866886e86d1138082e05dba02950463b6c221c
Chuck JS. Oh <jinseokoh@hotmail.com>
2022-08-29 12:02:38 +0900

    feature) update certification response properties

--- lib/response/Certification.ts
+++ lib/response/Certification.ts
@@ -1,19 +1,22 @@
 import ResponseBase from './Base';
 
 class Certification extends ResponseBase {
-  public imp_uid: string;
-  public merchant_uid?: string;
-  public pg_tid?: string;
-  public pg_provider?: string;
-  public name?: string;
-  public gender?: string;
-  public birth?: Date | number;
-  public foreigner?: boolean;
-  public certified?: boolean;
-  public certified_at?: Date | number;
-  public unique_key?: string;
-  public unique_in_site?: string;
-  public origin?: string;
+  public imp_uid: string
+  public merchant_uid?: string
+  public pg_tid?: string
+  public pg_provider?: string
+  public name?: string
+  public gender?: string
+  public birth?: Date | number
+  public foreigner?: boolean
+  public certified?: boolean
+  public certified_at?: Date | number
+  public unique_key?: string
+  public unique_in_site?: string
+  public origin?: string
+  public birthday?: string
+  public phone?: string
+  public carrier?: string
 
   public setAttributes(response: Certification): void {
     const {
@@ -30,20 +33,26 @@ class Certification extends ResponseBase {
       unique_key,
       unique_in_site,
       origin,
-    } = response;
-    this.imp_uid = imp_uid;
-    this.merchant_uid = merchant_uid;
-    this.pg_tid = pg_tid;
-    this.pg_provider = pg_provider;
-    this.name = name;
-    this.gender = gender;
-    this.birth = this.timeToDate(birth);
-    this.foreigner = foreigner;
-    this.certified = certified;
-    this.certified_at = this.timeToDate(certified_at);
-    this.unique_key = unique_key;
-    this.unique_in_site = unique_in_site;
-    this.origin = origin;
+      birthday,
+      phone,
+      carrier,
+    } = response
+    this.imp_uid = imp_uid
+    this.merchant_uid = merchant_uid
+    this.pg_tid = pg_tid
+    this.pg_provider = pg_provider
+    this.name = name
+    this.gender = gender
+    this.birth = this.timeToDate(birth)
+    this.foreigner = foreigner
+    this.certified = certified
+    this.certified_at = this.timeToDate(certified_at)
+    this.unique_key = unique_key
+    this.unique_in_site = unique_in_site
+    this.origin = origin
+    this.birthday = birthday
+    this.phone = phone
+    this.carrier = carrier
   }
 }
 
